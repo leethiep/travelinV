f8edac28b2300553a82075fb1dda6ab00e3c3c8a
TaiTai3006 <101327980+TaiTai3006@users.noreply.github.com>
2022-11-18 01:15:43 +0700

    update 1.0.5

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { createContext, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./Pages/Home";
+import ReadBlogs from "./Pages/ReadBlogs";
+import ProtectedRoutes from "./components/PrivateRouter";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register"
+// import PrivateRoutes from "./PrivateRoutes";
+// import ProtectedRoutes from "./PrivateRouter";
+// import Personal from "./Personal";
+// import LoginPage from "./Login";
+// import Register from "./Register";
+export const UserContext = createContext();
+
+function App() {
+  const [user, setUser] = useState({ loggedIn: false });
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<Home />} />
+          <Route path="/Blogs" element={<ReadBlogs />} />
+          {/* <Route path="/Blogs/:relatedPostID" element={<Blogs />} /> */}
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+          <Route element={<ProtectedRoutes />}>
+            {/* <Route path="/personal" element={<Personal />} /> */}
+          </Route>
+        </Route>
+      </Routes>
+    </UserContext.Provider>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./Pages/Home";
-import ReadBlogs from "./Pages/ReadBlogs";
-import Login from "./Pages/Login";
-import Register from "./Pages/Register";
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Header />}>
-          <Route index element={<Home />} />
-          <Route path="Blogs" element={<ReadBlogs />} />
-          <Route path="/Login" element={<Login />} />
-          <Route path="/Register" element={<Register/>}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
