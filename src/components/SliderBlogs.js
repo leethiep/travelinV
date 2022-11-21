5aec9bd426dfb08da1e049b84e80ff3b5212fdb8
sontran-test <sontran347123@gmail.com>
2022-11-21 23:48:03 +0700

    editslider

--- src/components/SliderBlogs.js
+++ src/components/SliderBlogs.js
@@ -2,6 +2,25 @@ import { useState } from "react";
 import React from "react";
 import { HiChevronRight } from "react-icons/hi";
 import { HiChevronLeft } from "react-icons/hi";
+import { Link } from "react-router-dom";
+const relatedPosts = [
+  {
+    image:
+      "https://i.pinimg.com/564x/da/ca/ec/dacaecc0b7fe84c6bebf8c142033f265.jpg",
+    title: "Địa điểm săn mây Đà Lạt",
+  },
+  {
+    image:
+      "https://i.pinimg.com/564x/6b/30/ef/6b30efc71a14e507e3821e5e6330ad30.jpg",
+    title: "Đà Lạt và những cung đường thơ mộng",
+  },
+  {
+    image:
+      "https://i.pinimg.com/564x/af/b6/bd/afb6bd2c6c3efcb3fec27362febb8a58.jpg",
+    title: "Bộ ảnh Đà Lạt qua đôi mắt của những “kẻ mộng mơ",
+  },
+];
+
 
 const slideImages = [
     {
@@ -11,13 +30,13 @@ const slideImages = [
     },
     {
       url: 'https://v-biz.vn/static/media/Travel.ff88c854.jpg',
-      titile:'da la ne2',
-      des: 'de di da lat thi ban can nhung gi2'
+      titile:'chim chich bong',
+      des: 'chim chich bong rat hay treo tu canh na sang canh buoi'
     },
     {
       url: 'https://e4life.vn/wp-content/uploads/2021/09/tu-vung-ielts-chu-de-travel.png',
-      titile:'da la ne3',
-      des: 'de di da lat thi ban can nhung gi3'
+      titile:'con co',
+      des: 'con co may di an dem dau phai canh mem lon co xuong ao'
     },
     {
       url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbK9ZHYnPY39soqR7NBQjuvykQwq2JSy1RuA&usqp=CAU',
@@ -36,6 +55,23 @@ const [currentSlide, setCurrentSlide] = useState(0)
     }
     return(
         <div>
+          <div className="realated-post-ctn">
+          <div className="sidebar-articles">
+          <div className="sidebar-articles_title">Related Posts</div>
+          <div className="sidebar-articles_container">
+            {relatedPosts.map((relatedPost, index) => (
+              <Link className="article-text-block" to="/Blogs/Dalat">
+                <div className="article-text-block_image" key={index}>
+                  <img src={relatedPost.image} alt="image of post"></img>
+                </div>
+                <div className="article-text-block_content">
+                  <h2>{relatedPost.title}</h2>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
           <button className=" slider-btn" id="left" onClick={prevSlide} ><HiChevronLeft/></button>
           <button className=" slider-btn" id="right" onClick={nextSlide} ><HiChevronRight/></button>
           <span className="slider-title">{slideImages[currentSlide].titile}</span>
@@ -47,8 +83,11 @@ const [currentSlide, setCurrentSlide] = useState(0)
                   </div>
             )
             )}
-          <button className="slider-button">Subtain Travel</button>
-        </div>
+          <a className="slider-button" href={slideImages.titile}>Go to post</a>
+
+          </div>
+
+
     );
 }
 export default Slider
